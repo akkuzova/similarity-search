d307b28c0a365dd5d87f05448a20c33cad4b844a
akkuzova <nactaiaa@gmail.com>
2023-06-23 23:36:56 -0700

    fix naming

--- app.py
+++ app.py
@@ -3,19 +3,18 @@ import json
 from flask import Flask
 from flask import request
 from text_index import TextIndex
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 app = Flask(__name__)
-textIndex = TextIndex()
+text_index = TextIndex()
 
 
 @app.post("/index")
 def create_index():
     vectors = json.load(request.json)
-    textIndex.build_index(vectors)
+    text_index.build_index(vectors)
     info = {
-        'vector_count': textIndex.get_vector_count(),
-        'dimension': textIndex.get_vector_dimension()
+        'vector_count': text_index.get_vector_count(),
+        'dimension': text_index.get_vector_dimension()
     }
     return info
 
@@ -24,4 +23,4 @@ def create_index():
 def get_keywords():
     xq = request.args.get('xq', '')
     k = request.args.get('k', '')
-    return textIndex.search(xq, k)
+    return text_index.search(xq, k)
